e67a65d122a901358a37f6f61cd68a640f83c8ba
Will Yang <wkyjyy@gmail.com>
2019-08-27 17:20:45 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,13 +13,13 @@ time build/tools/bv/bv --chc-file="bench_horn_bv/pipe-4.smt2" --const-bw=2 --ant
 
 
 #pass
-time build/tools/bv/bv --chc-file="bench_horn_bv/aes-1.smt2" --const-bw=4 --ante-size=1 --conseq-size=2 --conseq-disj=1 --grammar-file="bench_horn_bv/aes.gmr" > logs/aes-1.log
-time build/tools/bv/bv --chc-file="bench_horn_bv/aes-2.smt2" --const-bw=4 --ante-size=1 --conseq-size=2 --conseq-disj=1 --grammar-file="bench_horn_bv/aes.gmr" > logs/aes-2.log
-time build/tools/bv/bv --chc-file="bench_horn_bv/aes-3.smt2" --const-bw=4 --ante-size=1 --conseq-size=2 --conseq-disj=1 --grammar-file="bench_horn_bv/aes.gmr" > logs/aes-3.log
-time build/tools/bv/bv --chc-file="bench_horn_bv/aes-4.smt2" --const-bw=4 --ante-size=1 --conseq-size=2 --conseq-disj=1 --grammar-file="bench_horn_bv/aes.gmr" > logs/aes-4.log
+time build/tools/bv/bv --chc-file="bench_horn_bv/aes-1.smt2" --const-bw=4 --ante-size=1 --conseq-size=1 --conseq-disj=1 --grammar-file="bench_horn_bv/aes.gmr" > logs/aes-1.log
+time build/tools/bv/bv --chc-file="bench_horn_bv/aes-2.smt2" --const-bw=4 --ante-size=1 --conseq-size=1 --conseq-disj=1 --grammar-file="bench_horn_bv/aes.gmr" > logs/aes-2.log
+time build/tools/bv/bv --chc-file="bench_horn_bv/aes-3.smt2" --const-bw=4 --ante-size=1 --conseq-size=1 --conseq-disj=1 --grammar-file="bench_horn_bv/aes.gmr" > logs/aes-3.log
+time build/tools/bv/bv --chc-file="bench_horn_bv/aes-4.smt2" --const-bw=4 --ante-size=1 --conseq-size=1 --conseq-disj=1 --grammar-file="bench_horn_bv/aes.gmr" > logs/aes-4.log
 
-#fail
-time build/tools/bv/bv --chc-file="bench_horn_bv/aes-5.smt2" --const-bw=4 --ante-size=1 --conseq-size=2 --conseq-disj=1 --grammar-file="bench_horn_bv/aes.gmr" > logs/aes-5.log
+#pass (need unroll 10), don't try yet.
+time build/tools/bv/bv --chc-file="bench_horn_bv/aes-5.smt2" --const-bw=4 --ante-size=1 --conseq-size=1 --conseq-disj=1 --grammar-file="bench_horn_bv/aes.gmr" > logs/aes-5.log
 
 
 
